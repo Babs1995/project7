9e93d6a6c0de5b247226ab05cc1341b355ee1156
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-07-29 19:57:23 -0700

    fixing react stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import  React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import axios from 'axios'; 
 import apiKey from './config';
-import SearchForm from './components/SearchForm';
-import Results from './components/Results';
-import NotFound from './components/NotFound';
-import Nav from './components/Nav';
+import SearchForm from './component/SearchForm';
+import Results from './component/Results';
+import NotFound from './component/NotFound';
+import Nav from './component/Nav';
 
 
 class App extends Component {
@@ -20,7 +20,7 @@ class App extends Component {
       loading: true
     };
   } 
-  componentsDidMount(){
+  componentDidMount(){
     this.performSearch();
     this.performSearch('cats'); 
     this.performSearch('autumn'); 
@@ -29,14 +29,14 @@ class App extends Component {
 
   performSearch = (query ='cats')=>{
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-    .then(res => res.json())
+    .then(response => response.json())
     .then((data) => {
       if (query === 'cats') {
-        this.setState({travel: data.photos.photo, loading: false});
+        this.setState({cats: data.photos.photo, loading: false});
       } else if (query === 'autumn') {
-        this.setState({penguins: data.photos.photo, loading: false});
+        this.setState({autumn: data.photos.photo, loading: false});
       } else if (query === 'coffee') {
-        this.setState({flowers: data.photos.photo, loading: false});
+        this.setState({coffee: data.photos.photo, loading: false});
       } else {
         this.setState({
           photos: data.photos.photo,
@@ -59,13 +59,15 @@ render(){
       (this.state.loading)
       ?<p>Loading...</p>
       :<Switch>   
-      <Route exact path="/" component={() => <Results to="/cats" /> } />
-      <Route path="/cats" component={() => <Results query="cats" title="cats" data={this.state.cats} />} />
-      <Route path="/autumn" component={() => <Results query="autumn" title="autumn" data={this.state.autumn} />} />
-      <Route path="/coffee" component={() => <Results query="coffee" title="coffee" data={this.state.coffee} />} />
-      <Route path="/search/:query/" component={() => <Results query={this.state.query} data={this.state.photos} title= {this.state.query}/>} />
-      <Route component={NotFound} />
-        </Switch>
+
+        <Route exact path="/" component={() => <Results to="/cats" /> } />
+        <Route path="/cats" component={() => <Results query="cats" title="cats" data={this.state.cats} />} />
+        <Route path="/autumn" component={() => <Results query="autumn" title="autumn" data={this.state.autumn} />} />
+        <Route path="/coffee" component={() => <Results query="coffee" title="coffee" data={this.state.coffee} />} />
+        <Route path="/search/:query/" component={() => <Results query={this.state.query} data={this.state.photos} title= {this.state.query}/>} />
+        <Route component={NotFound} />
+
+      </Switch>
       </div>
     </BrowserRouter>
   );
